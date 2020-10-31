702a86919cc38f3233d94225ec47cd4b191faa63
Laszlo Treszkai <laszlo.treszkai@gmail.com>
2020-10-31 14:12:14 +0100

    conf: set static paths, change article paths to posts

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,10 +9,11 @@ THEME = 'theme'
 FILENAME_METADATA = r'(?P<date>\d{4}-\d\d-\d\d)-(?P<slug>.*)'
 
 PATH = 'content'
-ARTICLE_PATHS = ['blog']
+STATIC_PATHS = ['img']
+ARTICLE_PATHS = ['posts']
 ARTICLE_SAVE_AS = '{date:%Y/%m/%d}/{slug}.html'
 ARTICLE_URL = '{date:%Y/%m/%d}/{slug}.html'
-PAGE_PATHS = ['pages',  'legacy_redirects']
+PAGE_PATHS = ['pages', 'legacy_redirects']
 
 TIMEZONE = 'Europe/Paris'
 
